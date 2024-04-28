e6ba74b92bf49949456c92f9893c054be17b47b1
André Luiz <andre.doctors@gmail.com>
2024-04-27 23:33:33 -0400

    Feat: Criando classes Produ e Order

--- src/app/Programa.java
+++ src/app/Programa.java
@@ -1,6 +1,9 @@
 package app;
 
 import db.DB;
+import entities.Order;
+import entities.OrderStatus;
+import entities.Product;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -12,13 +15,36 @@ public class Programa {
 
         try(Connection conn = DB.getConnection();
             Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery("select * from tb_product")){
+            ResultSet rs = st.executeQuery("select * from tb_order")){
 
             while (rs.next()){
-                System.out.println(rs.getLong("Id") + ", " + rs.getString("Name"));
+                Order order = instantiateOrder(rs);
+                System.out.println(order);
             }
         }catch (SQLException e){
             e.printStackTrace();
         }
+    }
+
+    private static Order instantiateOrder(ResultSet rs) throws SQLException {
+        Order order = new Order();
+        order.setId(rs.getLong("id"));
+        order.setLatitude(rs.getDouble("latitude"));
+        order.setLongitude(rs.getDouble("longitude"));
+        order.setMoment(rs.getTimestamp("moment").toInstant());
+        order.setStatus(OrderStatus.values()[rs.getInt("status")]);
+
+        return order;
+    }
+
+    public static Product instantiateProduct(ResultSet rs) throws SQLException {
+        Product product = new Product();
+        product.setId(rs.getLong("id"));
+        product.setName(rs.getString("name"));
+        product.setDescription(rs.getString("description"));
+        product.setImageUrl(rs.getString("image_uri"));
+        product.setPrice(rs.getDouble("price"));
+
+        return product;
     }
 }
